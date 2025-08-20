fb08a1bb044969f99ca324ae7287c31b3344ea2a
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2025-08-20 15:22:28 +0300

    ignore tmux in idea and vscode terminals

--- tmux/tmux-bootstrap.sh
+++ tmux/tmux-bootstrap.sh
@@ -1,4 +1,4 @@
-if [[ -t 1 ]] && command -v tmux &> /dev/null && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+if [[ -t 1 ]] && command -v tmux &> /dev/null && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ] && [ -z "$VSCODE_INJECTION" ] && [ -z "$TERMINAL_EMULATOR" ]; then
 	tmux a || tmux new -s default
 	exit
 fi
