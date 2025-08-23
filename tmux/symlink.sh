257c68732662a52b73377d1d4ab34275889b5a78
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2025-08-23 20:15:11 +0300

    create dir if missing

--- tmux/symlink.sh
+++ tmux/symlink.sh
@@ -4,6 +4,7 @@ rm -f ~/.tmux.conf
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
 # NOTE: '~/.local/bin' must be on PATH (see .zshrc)
+mkdir -p ~/.local/bin
 rm -f ~/.local/bin/tmx
 chmod +x tmx
 ln -s $(pwd)/tmx ~/.local/bin/tmx
