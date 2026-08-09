ed0fee6b916aaca39655faf988a17ad5138fe398
Kook <102363876+andrii-k6a@users.noreply.github.com>
2026-08-09 15:04:59 +0300

    fix(tmux): skip bootstrap in Orca terminals

--- tmux/tmux-bootstrap.sh
+++ tmux/tmux-bootstrap.sh
@@ -1,4 +1,4 @@
-if [[ -t 1 ]] && command -v tmux &> /dev/null && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ] && [ "$TERM_PROGRAM" != "vscode" ] && [ -z "$TERMINAL_EMULATOR" ]; then
+if [[ -t 1 ]] && command -v tmux &> /dev/null && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ] && [ "$TERM_PROGRAM" != "vscode" ] && [ "$TERM_PROGRAM" != "Orca" ] && [ -z "$TERMINAL_EMULATOR" ]; then
 	tmux a || tmux new -s default
 	exit
 fi
